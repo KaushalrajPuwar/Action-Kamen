50d0afd2ace0222b23e80d7f7f9aaa0577cc7a2b
Kaushalraj Puwar <kaushalrajpuwar@gmail.com>
2024-04-07 15:37:28 +0530

    Revert "new register route"
    This reverts commit f3cb6952febe7c8714f4f658a9078c2ff6f996e6.

--- controllers/register.js
+++
@@ -1,3 +0,0 @@
-exports.get_register = function (req, res, next) {
-  res.render("register");
-};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV !== 'production'){
-    require('../.env').config()
-}
+// if (process.env.NODE_ENV !== 'production'){
+//     require('dotenv').config()
+// }
 
 var express = require('express');
 var bcrypt = require('bcrypt')
@@ -8,54 +8,47 @@ var passport = require('passport')
 var session = require('express-session')
 var flash = require('express-flash')
 var initpass = require("../passport-config.js");
-initpass(
-    passport, 
-    rollno => {return users.find(user => user.rollno == rollno)}
-)
+// initpass(passport, 
+//     rollno => {return users.find(user =>user.rollno == rollno)}
+// )
  
 var app = express();
 var router = express.Router();
-app.use(express.urlencoded({ extended: false }));
-app.use(flash)
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-}))
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(flash)
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false
+// }))
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 const userss = []
 
 let landing = require("../controllers/landing")
-let register = require("../controllers/register");
 let taxiPooling = require("../controllers/taxiPooling")
 let mySchedule = require("../controllers/mySchedule");
 let aboutUs = require("../controllers/aboutUs");
 
 /* GET home page. */
 router.get('/', landing.get_landing);
-router.post('/', passport.authenticate('local', {
-    successRedirect: '/taxiPooling',
-    failureRedirect: '/',
-    failureFlash: true
-}))
 
-/*GET register page */
-router.get("/register", register.get_register);
-router.post("/register", async (req, res) => {
-  {
-    const hashed = await bcrypt.hash(req.body.password, 10);
-    userss.push({
-      id: Date.now().toString(),
-      roll: req.body.rollno,
-      pwd: hashed,
-      type: req.body.formType,
-    });
-    console.log(userss);
-    res.redirect("/");
-  }
-});
+// router.post('/', async (req, res) => {
+//     if (req.body.formType == 'login')
+//     {const hashed = await bcrypt.hash(req.body.password, 10)
+//     userss.push({
+//         id: Date.now().toString(),
+//         roll: req.body.rollno,
+//         pwd: hashed,
+//         type: req.body.formType
+//     })
+//     console.log(userss)
+//     res.redirect("/aboutUs");}
+//     else{
+        
+//     }
+// })
 
 /* GET taxi page */
 router.get('/taxiPooling', taxiPooling.get_taxi)
