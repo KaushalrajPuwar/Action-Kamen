00e5eae85bebc646a9a84eadd63cfdc5740468e6
Kaushalraj Puwar <kaushalrajpuwar@gmail.com>
2024-04-07 15:38:58 +0530

    Revert "Small updates"
    This reverts commit 5ae8e930c811b8376b42fa98fdf5a3a5874a1732.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-// if (process.env.NODE_ENV !== 'production'){
-//     require('dotenv').config()
-// }
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
 
 var express = require('express');
 var bcrypt = require('bcrypt')
@@ -8,21 +8,21 @@ var passport = require('passport')
 var session = require('express-session')
 var flash = require('express-flash')
 var initpass = require("../passport-config.js");
-// initpass(passport, 
-//     rollno => {return users.find(user =>user.rollno == rollno)}
-// )
+initpass(passport, 
+    rollno => {return users.find(user =>user.rollno == rollno)}
+)
  
 var app = express();
 var router = express.Router();
-// app.use(express.urlencoded({ extended: false }));
-// app.use(flash)
-// app.use(session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false
-// }))
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+app.use(flash)
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
 const userss = []
 
@@ -34,21 +34,21 @@ let aboutUs = require("../controllers/aboutUs");
 /* GET home page. */
 router.get('/', landing.get_landing);
 
-// router.post('/', async (req, res) => {
-//     if (req.body.formType == 'login')
-//     {const hashed = await bcrypt.hash(req.body.password, 10)
-//     userss.push({
-//         id: Date.now().toString(),
-//         roll: req.body.rollno,
-//         pwd: hashed,
-//         type: req.body.formType
-//     })
-//     console.log(userss)
-//     res.redirect("/aboutUs");}
-//     else{
+router.post('/', async (req, res) => {
+    if (req.body.formType == 'login')
+    {const hashed = await bcrypt.hash(req.body.password, 10)
+    userss.push({
+        id: Date.now().toString(),
+        roll: req.body.rollno,
+        pwd: hashed,
+        type: req.body.formType
+    })
+    console.log(userss)
+    res.redirect("/aboutUs");}
+    else{
         
-//     }
-// })
+    }
+})
 
 /* GET taxi page */
 router.get('/taxiPooling', taxiPooling.get_taxi)
